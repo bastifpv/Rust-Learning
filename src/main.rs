da71fd730d4a07e4ccd34dc96e065769f8d36a82
Bastian Reuther <bastir25@icloud.com>
2025-02-02 22:04:49 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    const VALUE_CONST :u32 = 66;
+    println!("Hello, world!");
+    let mut x = 5;
+    println!("X:{:?}", x);
+    x = 3;
+    println!("X:{}", x);
+    let y = 3;
+    println!("Y:{}", y);
+    let y = 4;
+    println!("Y:{}", y);
+}
